81701ee947ee6ed66106e7c153967a5d9d288d05
Jeronimo Mendes <jeronimo.m.mendes@gmail.com>
2021-12-06 18:33:35 +0000

    Add main file

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void solve_problem_1();
+void solve_problem_2();
+
+int main() {
+
+    int problem;
+    cin >> problem;
+
+    if (problem == 1) solve_problem_1();
+    else if (problem == 2) solve_problem_2();
+
+    return 0;
+}
+
+void solve_problem_1() {
+    // TODO: problem 1 solution
+}
+
+void solve_problem_2() {
+    // TODO: problem 2 solution
+}
