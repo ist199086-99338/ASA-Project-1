3208b409b3c3877d0ccc43fb82621695bbecc98a
Jeronimo Mendes <jeronimo.m.mendes@gmail.com>
2022-01-11 15:20:43 +0000

    Add linear pre-processing to problem 2
    Co-authored-by: Tomás Marques <teoemeaesse@users.noreply.github.com>

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <sstream>
 
 #include <vector>
+#include <unordered_set>
 
 using namespace std;
 
@@ -49,31 +50,36 @@ void longest_possible_subsequence(vector<int> x) {
     vector<duplo> p = vector<duplo> (x.size(), {1, 1});
 
     size_t result = 1;
-    size_t repeated = x.size();
+    size_t repeated = 0;
+    duplo tuple_i, tuple_j;
+    size_t size_x = x.size();
 
-    for (size_t i = 0; i < x.size(); i++) {
+    for (size_t i = 0; i < size_x; i++) {
         // ler aqui
+        tuple_i = p[i];
+        int x_i = x[i];
         for (size_t j = 0; j < i; j++) {
-            if (x[i] > x[j]) { // then there's a new subvector with one more element
-                if (p[j].v + 1 == p[i].v) // esta a repetir o valor
-                    p[i].n += p[j].n;
+            if (x_i > x[j]) { // then there's a new subvector with one more element
+                tuple_j = p[j];
+                if (tuple_j.v + 1 == tuple_i.v) // esta a repetir o valor
+                    tuple_i.n += tuple_j.n;
                 
-                else if (p[j].v + 1 > p[i].v) { // esta a subir no size da sublista
-                    p[i].v = p[j].v + 1; // nova lista tem tamanho +1
-                    p[i].n = p[j].n;     // ha p[j].n listas de tamanho p[i].v
+                else if (tuple_j.v + 1 > tuple_i.v) { // esta a subir no size da sublista
+                    tuple_i.v = tuple_j.v + 1; // nova lista tem tamanho +1
+                    tuple_i.n = tuple_j.n;     // ha p[j].n listas de tamanho p[i].v
                 }
-
-                if(p[i].v > result) // result vai ser updated
-                    result = p[i].v;
             }
         }
+        if (tuple_i.v > result) {
+            result = tuple_i.v;
+            repeated = tuple_i.n;
+        } // result vai ser updated
+        else if (tuple_i.v == result) {
+            repeated += tuple_i.n;
+        }
+        p[i] = tuple_i;
     }
 
-    repeated = 0;
-    for (size_t i = 0; i < p.size(); i++) {
-        if (p[i].v == result)
-            repeated += p[i].n;
-    }
     cout << result << " " << repeated << endl;
 }
 
@@ -81,17 +87,24 @@ void lcis(vector<int> s1, vector<int> s2) {
     int res, var;
     size_t size_i = s1.size();
     size_t size_j = s2.size();
+    unordered_set <int> set_s2;
+    for (size_t i = 0; i < s2.size(); i++) {
+        set_s2.insert(s2[i]);
+    }
+
 
     // Guarda o valor da l.cis a cada index;
     vector<int> p = vector<int> (size_j, 0);
 
     for (size_t i = 0; i < size_i; i++) {
         var = 0;
-        for (size_t j = 0; j < size_j; j++) {
-            if (s1[i] == s2[j] && p[j] < var + 1) // found a common number, update the vector
-                p[j] = var + 1;
-            else if (s1[i] > s2[j] && p[j] > var)
-                var = p[j];
+        if (set_s2.find(s1[i]) != set_s2.end()) {
+            for (size_t j = 0; j < size_j; j++) {
+                if (s1[i] == s2[j] && p[j] < var + 1) // found a common number, update the vector
+                    p[j] = var + 1;
+                else if (s1[i] > s2[j] && p[j] > var)
+                    var = p[j];
+            }
         }
     }
 
@@ -102,48 +115,6 @@ void lcis(vector<int> s1, vector<int> s2) {
     }
 
     cout << res << endl;
-}
-
-
-void lcis2(vector<int> s1, vector<int> s2) {
-    vector< vector<duplo> > table = vector< vector<duplo> > (s1.size() + 1, vector<duplo>(s2.size() + 1, {0, 0}));
-
-    for(size_t i = 1; i <= s1.size(); i++) {
-        for(size_t j = 1; j <= s2.size(); j++) {
-            duplo left = table[i][j - 1];
-            duplo top = table[i - 1][j];
-            duplo *position = &table[i][j];
-
-            // numbers at index i and j are different
-            if(s1[i - 1] != s2[j - 1]) {
-            
-                // copy from left (j-1) if its last number (n) is lower than up's (i-1), copy from up otherwise
-                if(top.v == left.v)
-                    *position = top.n > left.n ? left : top;
-                
-                // copy from left (j-1) if its max sequence length (v) is higher than up's (i - 1), copy from up otherwise
-                else
-                    *position = top.v > left.v ? top : left;
-            }
-
-            // numbers at index i and j are equal
-            else {
-                if(left.v > top.v && (int) left.n < s1[i - 1])
-                    *position = {left.v + 1, (size_t) s1[i - 1]};
-                    
-                else {
-                    for(int k = i - 1; k >= 0; k--) {
-                        if((int) table[k][j].n < s1[i - 1]) {
-                            *position = {table[k][j].v + 1, (size_t) s1[i - 1]};
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    cout << table[s1.size()][s2.size()].v << endl;
 }
 
 void solve_problem_1() {
